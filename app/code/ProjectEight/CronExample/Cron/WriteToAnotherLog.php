73f715809055673481a3725e226b11525547875f
ProjectEight <s.frost@frostnet.co.uk>
2017-03-30 15:19:29 +0100

    Added cron config XML and action model classes

---
+++ app/code/ProjectEight/CronExample/Cron/WriteToAnotherLog.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * ProjectEight
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact ProjectEight for more information.
+ *
+ * @category    magento2-sample-modules.localhost.com
+ * @package     ProjectEight\CronExample\Cron
+ * @copyright   Copyright (c) 2017 ProjectEight
+ * @author      ProjectEight
+ *
+ */
+
+namespace ProjectEight\CronExample\Cron;
+
+use Psr\Log\LoggerInterface;
+
+/**
+ * CronExample cron actions
+ */
+class WriteToAnotherLog
+{
+    /**
+     * Logger
+     *
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * WriteToLog constructor
+     *
+     * @param LoggerInterface $logger
+     */
+    public function __construct(
+        LoggerInterface $logger
+    ) {
+        $this->logger = $logger;
+    }
+
+    /**
+     * Write to log. By default writes to var/log/system.log
+     *
+     * @return \ProjectEight\CronExample\Cron\WriteToAnotherLog
+     */
+    public function execute()
+    {
+        $this->logger->info("This message came from " . __METHOD__);
+
+        return $this;
+    }
+}
